59847546a7515a1e753ad9a591bd885468a2bc10
OscarLien <53479742+OscarLien@users.noreply.github.com>
2020-11-12 10:23:23 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env sh
 
-# 当发生错误时中止脚本
+# abort on errors
 set -e
 
-# 构建
+# build
 npm run build
 
-# cd 到构建输出的目录下 
+# navigate into the build output directory
 cd dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:OscarLien/vue_cli3_shopping.git master:gh-pages
+git push -f https://github.com/OscarLien/vue_cli3_shopping.git master:gh-pages
 
 cd -
